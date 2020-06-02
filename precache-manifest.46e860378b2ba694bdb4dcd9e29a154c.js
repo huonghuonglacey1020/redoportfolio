460877f529467850943d93a00fd51ece45ea3ee4
huongnguyen <huonghuonglacey1020@gmail.com>
2020-06-02 16:35:05 -0700

    Updates

---
+++ precache-manifest.46e860378b2ba694bdb4dcd9e29a154c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d6c29fd9523d3e063badbee7a1dfe992",
+    "url": "/huonghuonglacey1020/redoportfolio/index.html"
+  },
+  {
+    "revision": "755ebdd828570a98283a",
+    "url": "/huonghuonglacey1020/redoportfolio/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "d0b1ef99e44e38cb5428",
+    "url": "/huonghuonglacey1020/redoportfolio/static/css/main.8b97dfe1.chunk.css"
+  },
+  {
+    "revision": "755ebdd828570a98283a",
+    "url": "/huonghuonglacey1020/redoportfolio/static/js/2.da453778.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/huonghuonglacey1020/redoportfolio/static/js/2.da453778.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d0b1ef99e44e38cb5428",
+    "url": "/huonghuonglacey1020/redoportfolio/static/js/main.3f4d1cd2.chunk.js"
+  },
+  {
+    "revision": "b251d43b2135ffd0742e",
+    "url": "/huonghuonglacey1020/redoportfolio/static/js/runtime-main.cfb73204.js"
+  },
+  {
+    "revision": "46a0ed096fd0b6548bd87c817f24f7d8",
+    "url": "/huonghuonglacey1020/redoportfolio/static/media/profile.46a0ed09.jpg"
+  }
+]);
